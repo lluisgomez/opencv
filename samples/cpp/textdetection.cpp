636c9dbaa6e2d7c355ca438f6230341790611397
lluis <lgomez@cvc.uab.es>
2014-03-16 13:11:58 +0100

    added timers for time cost measurement

--- samples/cpp/textdetection.cpp
+++ samples/cpp/textdetection.cpp
@@ -44,7 +44,7 @@ int  main(int argc, const char * argv[])
         channels.push_back(255-channels[c]);
 
     // Create ERFilter objects with the 1st and 2nd stage default classifiers
-    Ptr<ERFilter> er_filter1 = createERFilterNM1(loadClassifierNM1("trained_classifierNM1.xml"),16,0.00015f,0.13f,0.2f,true,0.1f);
+    Ptr<ERFilter> er_filter1 = createERFilterNM1(loadClassifierNM1("trained_classifierNM1.xml"),1,0.00015,0.13,0.2,true,0.1);
     Ptr<ERFilter> er_filter2 = createERFilterNM2(loadClassifierNM2("trained_classifierNM2.xml"),0.5);
 
     vector<vector<ERStat> > regions(channels.size());
@@ -53,14 +53,23 @@ int  main(int argc, const char * argv[])
     cout << "    (...) this may take a while (...)" << endl << endl;
     for (int c=0; c<(int)channels.size(); c++)
     {
+	    	double t = (double)cvGetTickCount();
         er_filter1->run(channels[c], regions[c]);
+	    	t = cvGetTickCount() - t;
+	    	printf( "filter 1 done in %g ms.\n", t/((double)cvGetTickFrequency()*1000.) );
+	    	t = (double)cvGetTickCount();
         er_filter2->run(channels[c], regions[c]);
+	    	t = cvGetTickCount() - t;
+	    	printf( "filter 2 done in %g ms.\n", t/((double)cvGetTickFrequency()*1000.) );
     }
 
     // Detect character groups
     cout << "Grouping extracted ERs ... ";
     vector<Rect> groups;
+	  double t = (double)cvGetTickCount();
     erGrouping(channels, regions, "trained_classifier_erGrouping.xml", 0.5, groups);
+	  t = cvGetTickCount() - t;
+	  printf( "Grouping done in %g ms.\n", t/((double)cvGetTickFrequency()*1000.) );
 
     // draw groups
     groups_draw(src, groups);
@@ -94,7 +103,7 @@ void show_help_and_exit(const char *cmd)
 
 void groups_draw(Mat &src, vector<Rect> &groups)
 {
-    for (int i=(int)groups.size()-1; i>=0; i--)
+    for (int i=groups.size()-1; i>=0; i--)
     {
         if (src.type() == CV_8UC3)
             rectangle(src,groups.at(i).tl(),groups.at(i).br(),Scalar( 0, 255, 255 ), 3, 8 );
